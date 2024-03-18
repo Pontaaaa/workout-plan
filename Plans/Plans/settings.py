41efb786ff3913145357b12ae7dfe90dacd57ccc
Pontaaaa <jonathan.stenstrom@hotmail.com>
2024-03-18 22:04:52 +0100

    feature so login no longer shows up when logged in

--- Plans/Plans/settings.py
+++ Plans/Plans/settings.py
@@ -62,10 +62,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Plans.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Plans/Plans/urls.py
+++ Plans/Plans/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
